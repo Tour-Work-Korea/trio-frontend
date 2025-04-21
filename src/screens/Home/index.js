47f707fd65766ca6dc81e84bfb2590b576c181f8
eeazzy <khs2498@gmail.com>
2025-04-21 22:06:01 +0900

    feat: 버튼 2개 컴포넌트로 구현 #1

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, StatusBar, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { COLORS } from '@constants/colors';
-import Logo from '@assets/images/Logo.svg';
+import Logo from '@assets/images/Logo.svg'; // svg이미지 불러오기
 import styles from './Home.styles';
+import ButtonScarlet from '@components/ButtonScarlet'; // 버튼 컴포넌트 불러오기
+import ButtonWhite from '@components/ButtonWhite';
 
 const Home = () => {
   const navigation = useNavigation();
 
   const goToLogin = () => {
     navigation.navigate('Login');
-  };
+  }; // 화면 이동 예시 함수
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={COLORS.bb} />
       <View style={styles.header}>
         <Text style={styles.title}>🧡 Welcome to Trio App</Text>
       </View>
       <View style={styles.body}>
-        <Logo width={100} height={100} />
-        <Text style={styles.text}>이 홈 화면은 COLORS 상수로 스타일링된 예시</Text>
-        <Button title="로그인 페이지로 이동" onPress={goToLogin} />
+        <Logo width={100} height={100} />  {/* svg 이미지 */}
+        <Text style={styles.text}>이 홈 화면은 예시 화면</Text>
       </View>
+      <ButtonScarlet title="로그인 페이지로 이동" to="Login" /> {/* 버튼 컴포넌트 사용 */}
+      <ButtonWhite title="로그인 페이지로 이동" to="Login" />
     </SafeAreaView>
   );
 };
