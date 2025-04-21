38cf06a85f4205365d935c9a907c507f7bd8dbcf
eeazzy <khs2498@gmail.com>
2025-04-21 21:27:54 +0900

    feat: COLORS에 사용색 추가

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,5 +2,8 @@
 export const COLORS = {
     white: '#FFFFFF',
     black: '#000000',
-    bb: '#FF6F61',
+    scarlet: '#FF6F61',
+    light_gray: '#F8F9FB',
+    stroke_gray: '#C8D1E2',
+    gray: '#747D8F',
 };
