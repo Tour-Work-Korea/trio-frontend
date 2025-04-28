b99eed7285ca1cfcaf00ea5f2f532473c0c90792
eeazzy <khs2498@gmail.com>
2025-04-29 02:50:09 +0900

    feat: 사장님, 유저 각각 개인정보 화면 구현 #4

--- src/screens/(Host)/HostMyPage/HostEditProfile/index.js
+++ src/screens/(Host)/HostMyPage/HostEditProfile/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+import PersonIcon from '@assets/images/Gray_Person.svg';
+import PlusIcon from '@assets/images/plus.svg';
+import RightArrow from '@assets/images/gray_chevron_right.svg';
+
+import Header from '@components/Header';
+import styles from './HostEditProfile.styles';
+import { FONTS } from '@constants/fonts';
 
 const HostEditProfile = () => {
   return (
-    <View>
-      <Text>HostEditProfile Screen</Text>
+    <View style={styles.outContainer}>
+      <Header title="마이페이지" />
+      <View style={styles.container}>
+
+        {/* 프로필 영역 */}
+        <View style={styles.profileContainer}>
+          <View style={styles.profileImageWrapper}>
+            <View style={styles.profileImage}>
+              <PersonIcon width={36} height={36} />
+            </View>
+            <TouchableOpacity style={styles.plusButton}>
+              <PlusIcon width={20} height={20} />
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.nameButton}>
+            <Text style={[FONTS.fs_h2_bold, styles.nameText]}>김사장</Text>
+            <RightArrow width={20} height={20} />
+          </TouchableOpacity>
+        </View>
+
+        {/* 정보 수정 리스트 */}
+        <View style={styles.infoContainer}>
+          <InfoItem label="휴대폰 번호" value="010-0000-0000" />
+          <InfoItem label="이메일 주소" value="1234@gmail.com" />
+          <InfoItem label="사업자 번호" value="0000000000" noArrow />
+        </View>
+
+      </View>
     </View>
   );
 };
+
+const InfoItem = ({ label, value, noArrow }) => (
+  <TouchableOpacity disabled={noArrow} style={styles.infoItem}>
+    <Text style={[FONTS.fs_h2_bold, styles.infoLabel]}>{label}</Text>
+    <View style={styles.infoRight}>
+      <Text style={[FONTS.fs_h2, styles.infoValue]}>{value}</Text>
+      {!noArrow && <RightArrow width={20} height={20} />}
+    </View>
+  </TouchableOpacity>
+);
 
 export default HostEditProfile;
--- src/screens/(User)/UserMyPage/UserEditProfile/index.js
+++ src/screens/(User)/UserMyPage/UserEditProfile/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+import PersonIcon from '@assets/images/Gray_Person.svg';
+import PlusIcon from '@assets/images/plus.svg';
+import RightArrow from '@assets/images/gray_chevron_right.svg';
+
+import Header from '@components/Header';
+import styles from './UserEditProfile.styles';
+import { FONTS } from '@constants/fonts';
 
 const UserEditProfile = () => {
   return (
-    <View>
-      <Text>UserEditProfile Screen</Text>
+    <View style={styles.outContainer}>
+      <Header title="마이페이지" />
+      <View style={styles.container}>
+
+        {/* 프로필 영역 */}
+        <View style={styles.profileContainer}>
+          <View style={styles.profileImageWrapper}>
+            <View style={styles.profileImage}>
+              <PersonIcon width={36} height={36} />
+            </View>
+            <TouchableOpacity style={styles.plusButton}>
+              <PlusIcon width={20} height={20} />
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.nameButton}>
+            <Text style={[FONTS.fs_h2_bold, styles.nameText]}>김이지</Text>
+            <RightArrow width={20} height={20} />
+          </TouchableOpacity>
+        </View>
+
+        {/* 정보 수정 리스트 */}
+        <View style={styles.infoContainer}>
+          <InfoItem label="휴대폰 번호" value="010-0000-0000" />
+          <InfoItem label="이메일 주소" value="1234@gmail.com" />
+          <InfoItem label="MBTI" value="ISTJ" />
+          <InfoItem label="인스타" value="@daaii" />
+        </View>
+
+      </View>
     </View>
   );
 };
 
+const InfoItem = ({ label, value, noArrow }) => (
+  <TouchableOpacity disabled={noArrow} style={styles.infoItem}>
+    <Text style={[FONTS.fs_h2_bold, styles.infoLabel]}>{label}</Text>
+    <View style={styles.infoRight}>
+      <Text style={[FONTS.fs_h2, styles.infoValue]}>{value}</Text>
+      {!noArrow && <RightArrow width={20} height={20} />}
+    </View>
+  </TouchableOpacity>
+);
+
 export default UserEditProfile;
+
