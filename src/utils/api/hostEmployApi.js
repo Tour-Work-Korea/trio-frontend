c14d39b951e7481e5900c8c3bdec478bc22b6b04
kwak <sal091625@gmail.com>
2025-05-24 16:40:12 +0900

    feat(#20): 사장님 구인구직 관련 api 함수 작성

---
+++ src/utils/api/hostEmployApi.js
@@ -0,0 +1,34 @@
+import api from './axiosInstance';
+
+const hostEmployApi = {
+  //사장님 채용공고 등록
+  createRecruit: createRecruitData =>
+    api.post('/host/recruits', createRecruitData),
+
+  //사장님 나의 채용공고 목록 조회
+  getMyRecruits: () => api.get('/host/recruits'),
+
+  //채용공고 상세 조회
+  getRecruitDetail: recruitId => api.get(`/host/recruits/${recruitId}`),
+
+  //채용공고 수정
+  updateRecruit: (recruitId, updateRecruitData) =>
+    api.put(`/host/recruits/${recruitId}`, updateRecruitData),
+
+  //채용공고 삭제 요청
+  requestDeleteRecruit: (recruitId, reason) =>
+    api.post(`/host/recruits/${recruitId}`, reason),
+
+  //지원서 전체 조회
+  getAllApplicants: () => api.get('/host/recruits/applications'),
+
+  //게스트하우스 별 지원서 조회
+  getApplicantDetail: applicantId =>
+    api.get(`/host/recruits/applications/${applicantId}`),
+
+  //지원서 상세 조회
+  getApplicantsByGuesthouse: guesthouseId =>
+    api.get(`/host/recruits/applications/${guesthouseId}`),
+};
+
+export default hostEmployApi;
