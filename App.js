ded637fca7c2843ba29475ba1da5a716da058743
kwak <sal091625@gmail.com>
2025-07-11 19:59:41 +0900

    feat(#44): splash 화면 구현

--- App.js
+++ App.js
@@ -5,13 +5,33 @@ import RootNavigation from '@navigations/RootNavigation';
 import 'react-native-gesture-handler';
 import {COLORS} from '@constants/colors';
 import {tryAutoLogin} from '@utils/auth/login';
+import LottieView from 'lottie-react-native';
 
 const App = () => {
+  const [appLoaded, setAppLoaded] = useState(false);
   useEffect(() => {
     tryAutoLogin();
   }, []);
 
-  return (
+  return !appLoaded ? (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <LottieView
+        source={require('@assets/lottie/splash.json')}
+        style={{width: 180, height: 153}}
+        autoPlay
+        loop={false}
+        onAnimationFinish={() => {
+          setAppLoaded(true);
+        }}
+      />
+    </View>
+  ) : (
     <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle="light-content"
