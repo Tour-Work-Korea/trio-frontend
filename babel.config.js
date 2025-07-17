52f0bf318f0324842f2856003fa02f794e7d0160
cie10 <leesky0075@dgu.ac.kr>
2025-07-17 14:53:49 +0100

    fix: change file path

--- babel.config.js
+++ babel.config.js
@@ -22,7 +22,7 @@ module.exports = {
       'module:react-native-dotenv',
       {
         moduleName: '@env',
-        path: '.env',
+        path: 'ios/.env.production',
       },
     ],
     'react-native-reanimated/plugin',
