522141ecdf25a8c90396b6942bd47d47704dbfe2
eeazzy <khs2498@gmail.com>
2025-07-29 23:00:04 +0900

    feat(#10): 게하 검색, 출력 화면 수정 & 임시 빈 결과값 화면 구현

--- src/data/filter.js
+++ src/data/filter.js
@@ -1,7 +1,13 @@
 export const regions = [
   {
     name: '제주',
-    subRegions: ['제주전체', '제주북부', '제주동부', '제주남부', '제주서부'],
+    subRegions: [
+      { name: '제주전체', regionIds: [1, 2, 3, 4] },
+      { name: '제주북부', regionIds: [4] },
+      { name: '제주동부', regionIds: [1] },
+      { name: '제주남부', regionIds: [3] },
+      { name: '제주서부', regionIds: [2] },
+    ],
   },
-  // 지역 확정되면 나중에 데이터 파일로 빼기
+  // 지역 확정되면 나중에 더 추가
 ];
